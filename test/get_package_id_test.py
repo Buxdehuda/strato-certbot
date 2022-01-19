a039a789abdd905fbf5cb1ee0abb6ea236a87eaa
Buxdehuda <Buxdehuda@users.noreply.github.com>
2022-01-19 12:02:36 +0100

    split tests

--- test/get_package_id_test.py
+++ test/get_package_id_test.py
@@ -6,26 +6,25 @@ from certbotstratoapi import CertbotStratoApi
 
 
 @pytest.mark.parametrize("test_input,expected", [
-    ('domain-a.de', '7'),
-    ('*.domain-a.de', '7'),
+    ('domain-a.de', 'domain-a.de'),
+    ('*.domain-a.de', 'domain-a.de'),
+    ('test.domain-a.de', 'domain-a.de')
+])
+def test_parse_second_level_domain(test_input, expected):
+    os.environ['CERTBOT_DOMAIN'] = test_input
+    os.environ['CERTBOT_VALIDATION'] = 'xyz'
+
+    strato = CertbotStratoApi()
+
+    assert strato.second_level_domain_name == expected
+
+@pytest.mark.parametrize("test_input,expected", [
     ('test.domain-a.de', '7'),
-    ('domain-b.de', '6'),
-    ('*.domain-b.de', '6'),
     ('test.domain-b.de', '6'),
-    ('domain-c.de', '5'),
-    ('*.domain-c.de', '5'),
     ('test.domain-c.de', '5'),
-    ('domain-d.de', '4'),
-    ('*.domain-d.de', '4'),
     ('test.domain-d.de', '4'),
-    ('domain-e.de', '2'),
-    ('*.domain-e.de', '2'),
     ('test.domain-e.de', '2'),
-    ('domain-f.de', '2'),
-    ('*.domain-f.de', '2'),
     ('test.domain-f.de', '2'),
-    ('domain-g.de', '1'),
-    ('*.domain-g.de', '1'),
     ('test.domain-g.de', '1')
 ])
 def test_get_package_id(test_input, expected, requests_mock):
