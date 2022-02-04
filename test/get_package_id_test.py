0e658414b43f2d7ccff4360bdf7e44ee9bd81816
Buxdehuda <Buxdehuda@users.noreply.github.com>
2022-02-04 20:19:08 +0100

    parametrize html test file

--- test/get_package_id_test.py
+++ test/get_package_id_test.py
@@ -19,17 +19,17 @@ def test_parse_second_level_domain(test_input, expected):
     assert strato.second_level_domain_name == expected
 
 
-@pytest.mark.parametrize("test_input,expected", [
-    ('test.domain-a.de', '7'),
-    ('test.domain-b.de', '6'),
-    ('test.domain-c.de', '5'),
-    ('test.domain-d.de', '4'),
-    ('test.domain-e.de', '2'),
-    ('test.domain-f.de', '2'),
-    ('test.domain-g.de', '1')
+@pytest.mark.parametrize('test_file,test_input,expected', [
+    ('test/projectPage.html', 'test.domain-a.de', '7'),
+    ('test/projectPage.html', 'test.domain-b.de', '6'),
+    ('test/projectPage.html', 'test.domain-c.de', '5'),
+    ('test/projectPage.html', 'test.domain-d.de', '4'),
+    ('test/projectPage.html', 'test.domain-e.de', '2'),
+    ('test/projectPage.html', 'test.domain-f.de', '2'),
+    ('test/projectPage.html', 'test.domain-g.de', '1')
 ])
-def test_get_package_id(test_input, expected, requests_mock):
-    project_page = open('test/projectPage.html', 'r').read()
+def test_get_package_id(test_file, test_input, expected, requests_mock):
+    project_page = open(test_file, 'r').read()
     os.environ['CERTBOT_DOMAIN'] = test_input
     os.environ['CERTBOT_VALIDATION'] = 'xyz'
     strato = CertbotStratoApi()
