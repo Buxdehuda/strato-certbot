2a66c548a81271d4e6d93eae7b0caccdd33745d2
Luca Krawczyk <luca@calchera.de>
2024-10-25 00:15:07 +0200

    add auth.env to runscript

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --env-file certbot.env --rm -v letsencrypt:/etc/letsencrypt stratobot 
+docker run --env-file certbot.env --env-file auth.env --rm -v letsencrypt:/etc/letsencrypt stratobot
